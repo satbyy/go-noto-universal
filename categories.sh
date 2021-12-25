c289a7f6cc9c936f1f28d3684ba35a12135e517c
Satish B <unknown>
2021-12-25 21:52:51 +0000

    Move regional fonts' generation to pyftmerge
    - Copy generate.py arrays to categories.sh
    - Move "go_build()" to helper.sh so that it can be used for both
      regional_fonts.sh and temporal_fonts.sh
    - Create regional_fonts.sh to call these functions

--- categories.sh
+++ categories.sh
@@ -2,10 +2,17 @@
 set -e
 
 # export these variables
-declare -x current
-declare -x ancient
+declare -x GoNotoAncient
+declare -x GoNotoCurrent
 
-ancient=(
+declare -x GoNotoAfricaMiddleEast
+declare -x GoNotoSouthAsia
+declare -x GoNotoAsiaHistorical
+declare -x GoNotoSouthEastAsia
+declare -x GoNotoEastAsia
+declare -x GoNotoEuropeAmericas
+
+GoNotoAncient=(
     "NotoSans-Regular.ttf"
     # 'GoNotoAsiaHistorical.ttf': [
     "NotoSansBhaiksuki-Regular.ttf"
@@ -90,7 +97,7 @@ ancient=(
     "NotoMusic-Regular.ttf"
 )
 
-current=(
+GoNotoCurrent=(
     # It's recommended to put NotoSans-Regular.ttf as the first element in the
     # list to maximize the amount of meta data retained in the final merged font.
     # ------- South Asia ---------
@@ -176,6 +183,222 @@ current=(
     "NotoSansYi-Regular.ttf"
     "NotoSansNushuSubset-Regular.ttf" # Not exactly contemporary use but just 402 glyphs
     "NotoSansMiao-Regular.ttf"
+    # Common for all scripts
+    "NotoSansSymbols-Regular.ttf"
+    "NotoSansSymbols2-Regular.ttf"
+    "NotoSansMath-Regular.ttf"
+    "NotoMusic-Regular.ttf"
+)
+
+GoNotoAfricaMiddleEast=(
+    "NotoSans-Regular.ttf"
+    "NotoNaskhArabic-Regular.ttf" # or "NotoSansArabic-Regular.ttf"
+    "NotoSansAdlam-Regular.ttf"
+    "NotoSansBamum-Regular.ttf"
+    "NotoSansBassaVah-Regular.ttf"
+    "NotoSansHebrew-Regular.ttf"
+    "NotoSansSyriac-Regular.ttf"
+    "NotoSansSamaritan-Regular.ttf"
+    "NotoSansMandaic-Regular.ttf"
+    "NotoSerifYezidi-Regular.ttf"
+    # Historical scripts
+    "NotoSansOldNorthArabian-Regular.ttf"
+    "NotoSansOldSouthArabian-Regular.ttf"
+    "NotoSansPhoenician-Regular.ttf"
+    "NotoSansImperialAramaic-Regular.ttf"
+    "NotoSansManichaean-Regular.ttf"
+    "NotoSansInscriptionalParthian-Regular.ttf"
+    "NotoSansInscriptionalPahlavi-Regular.ttf"
+    "NotoSansPsalterPahlavi-Regular.ttf"
+    "NotoSansAvestan-Regular.ttf"
+    # "NotoSansChorasmian-Regular.ttf" # doesn't exist
+    "NotoSansElymaic-Regular.ttf"
+    "NotoSansNabataean-Regular.ttf"
+    "NotoSansPalmyrene-Regular.ttf"
+    "NotoSansHatran-Regular.ttf"
+    # Cuneiform and Hieroglyphs
+    "NotoSansCuneiform-Regular.ttf"
+    "NotoSansUgaritic-Regular.ttf"
+    "NotoSansOldPersian-Regular.ttf"
+    "NotoSansEgyptianHieroglyphs-Regular.ttf"
+    "NotoSansMeroitic-Regular.ttf"
+    "NotoSansAnatolianHieroglyphs-Regular.ttf"
+    # Africa
+    "NotoSansEthiopic-Regular.ttf"
+    "NotoSansOsmanya-Regular.ttf"
+    "NotoSansTifinagh-Regular.ttf" # TODO: check Tifinagh variants
+    "NotoSansNKo-Regular.ttf"
+    "NotoSansVai-Regular.ttf"
+    "NotoSansMendeKikakui-Regular.ttf"
+    "NotoSansMedefaidrin-Regular.ttf"
+    # Common for all scripts
+    "NotoSansSymbols-Regular.ttf"
+    "NotoSansSymbols2-Regular.ttf"
+    "NotoSansMath-Regular.ttf"
+    "NotoMusic-Regular.ttf"
+)
+
+GoNotoSouthAsia=(
+    "NotoSans-Regular.ttf"
+    "NotoNastaliqUrdu-Regular.ttf"
+    "NotoSansBengali-Regular.ttf"
+    "NotoSansChakma-Regular.ttf"
+    "NotoSansDevanagari-Regular.ttf"
+    "NotoSansGujarati-Regular.ttf"
+    "NotoSansGunjalaGondi-Regular.ttf"
+    "NotoSansGurmukhi-Regular.ttf"
+    "NotoSansKannada-Regular.ttf"
+    "NotoSansLepcha-Regular.ttf"
+    "NotoSansLimbu-Regular.ttf"
+    "NotoSansMalayalam-Regular.ttf"
+    "NotoSansMasaramGondi-Regular.ttf"
+    "NotoSansMeeteiMayek-Regular.ttf"
+    "NotoSansMro-Regular.ttf"
+    "NotoSansNewa-Regular.ttf"
+    "NotoSansOlChiki-Regular.ttf"
+    "NotoSansOriya-Regular.ttf"
+    "NotoSansSaurashtra-Regular.ttf"
+    "NotoSansSinhala-Regular.ttf"
+    "NotoSansTamil-Regular.ttf"
+    "NotoSansTelugu-Regular.ttf"
+    "NotoSansThaana-Regular.ttf"
+    "NotoSerifTibetanSubset-Regular.ttf"
+    "NotoSansWancho-Regular.ttf"
+    "NotoSansWarangCiti-Regular.ttf"
+    # Common for all scripts
+    "NotoSansSymbols-Regular.ttf"
+    "NotoSansSymbols2-Regular.ttf"
+    "NotoSansMath-Regular.ttf"
+    "NotoMusic-Regular.ttf"
+)
+
+GoNotoAsiaHistorical=(
+    "NotoSans-Regular.ttf"
+    "NotoSansBhaiksuki-Regular.ttf"
+    "NotoSansBrahmi-Regular.ttf"
+    "NotoSansGrantha-Regular.ttf"
+    "NotoSansKaithi-Regular.ttf"
+    "NotoSansKharoshthi-Regular.ttf"
+    "NotoSansKhudawadi-Regular.ttf"
+    "NotoSansMahajani-Regular.ttf"
+    "NotoSansMarchen-Regular.ttf"
+    "NotoSansModi-Regular.ttf"
+    "NotoSansMultani-Regular.ttf"
+    "NotoSansNandinagariSubset-Regular.ttf"
+    "NotoSansOldSogdian-Regular.ttf"
+    "NotoSansOldTurkic-Regular.ttf"
+    "NotoSansPhagsPa-Regular.ttf"
+    "NotoSansSharada-Regular.ttf"
+    "NotoSansSiddham-Regular.ttf"
+    "NotoSansSogdian-Regular.ttf"
+    "NotoSansSoraSompeng-Regular.ttf"
+    "NotoSansSoyombo-Regular.ttf"
+    "NotoSansSylotiNagri-Regular.ttf"
+    "NotoSansTakri-Regular.ttf"
+    "NotoSansTirhuta-Regular.ttf"
+    "NotoSansZanabazarSquare-Regular.ttf"
+    "NotoSansIndicSiyaqNumbers-Regular.ttf"
+    "NotoSerifAhom-Regular.ttf"
+    "NotoSerifDograSubset-Regular.ttf"
+    "NotoSerifKhojki-Regular.ttf"
+    "NotoSerifOldUyghur-Regular.ttf"
+    # Common for all scripts
+    "NotoSansSymbols-Regular.ttf"
+    "NotoSansSymbols2-Regular.ttf"
+    "NotoSansMath-Regular.ttf"
+    "NotoMusic-Regular.ttf"
+)
+
+GoNotoSouthEastAsia=(
+    "NotoSans-Regular.ttf"
+    "NotoSansBalinese-Regular.ttf"
+    "NotoSansBatak-Regular.ttf"
+    "NotoSansBuginese-Regular.ttf"
+    "NotoSansBuhid-Regular.ttf"
+    "NotoSansCham-Regular.ttf"
+    "NotoSansHanifiRohingya-Regular.ttf"
+    "NotoSansHanunoo-Regular.ttf"
+    "NotoSansJavanese-Regular.ttf"
+    "NotoSansKayahLi-Regular.ttf"
+    "NotoSansKhmer-Regular.ttf"
+    "NotoSansLao-Regular.ttf"
+    # "NotoSansMakasar-Regular.ttf" # doesn't exist
+    "NotoSansMyanmar-Regular.ttf"
+    "NotoSansNewTaiLue-Regular.ttf"
+    "NotoSansPahawhHmong-Regular.ttf"
+    "NotoSerifNyiakengPuachueHmong-Regular.ttf"
+    "NotoSansPauCinHau-Regular.ttf"
+    "NotoSansRejang-Regular.ttf"
+    "NotoSansSundanese-Regular.ttf"
+    "NotoSansTagalog-Regular.ttf"
+    "NotoSansTagbanwa-Regular.ttf"
+    "NotoSansTaiLe-Regular.ttf"
+    "NotoSansTaiTham-Regular.ttf"
+    "NotoSansTaiViet-Regular.ttf"
+    "NotoSansThai-Regular.ttf"
+    # Common for all scripts
+    "NotoSansSymbols-Regular.ttf"
+    "NotoSansSymbols2-Regular.ttf"
+    "NotoSansMath-Regular.ttf"
+    "NotoMusic-Regular.ttf"
+)
+
+GoNotoEastAsia=(
+    "NotoSans-Regular.ttf"
+    "NotoSansMarchen-Regular.ttf"
+    "NotoSansMongolianSubset-Regular.ttf"
+    "NotoSansOldSogdian-Regular.ttf"
+    "NotoSansOldTurkic-Regular.ttf"
+    "NotoSansPhagsPa-Regular.ttf"
+    "NotoSansSogdian-Regular.ttf"
+    "NotoSansSoyombo-Regular.ttf"
+    "NotoSansZanabazarSquare-Regular.ttf"
+    "NotoSerifTibetanSubset-Regular.ttf"
+    "NotoSansYi-Regular.ttf"
+    "NotoSansNushuSubset-Regular.ttf"
+    "NotoSansLisu-Regular.ttf"
+    "NotoSansMiao-Regular.ttf"
+    "NotoSerifTangutSubset-Regular.ttf"
+    # Common for all scripts
+    "NotoSansSymbols-Regular.ttf"
+    "NotoSansSymbols2-Regular.ttf"
+    "NotoSansMath-Regular.ttf"
+    "NotoMusic-Regular.ttf"
+)
+
+GoNotoEuropeAmericas=(
+    "NotoSans-Regular.ttf"
+    "NotoSansArmenian-Regular.ttf"
+    "NotoSansCherokee-Regular.ttf"
+    "NotoSansCoptic-Regular.ttf"
+    "NotoSansDeseret-Regular.ttf"
+    "NotoSansGeorgian-Regular.ttf"
+    "NotoSansGlagolitic-Regular.ttf"
+    "NotoSansOsage-Regular.ttf"
+    "NotoSansSignWriting-Regular.ttf"
+    "NotoSerifNyiakengPuachueHmong-Regular.ttf"
+    # Historical
+    "NotoSansCypriot-Regular.ttf"
+    "NotoSansCaucasianAlbanian-Regular.ttf"
+    "NotoSansCarian-Regular.ttf"
+    "NotoSansCanadianAboriginal-Regular.ttf"
+    "NotoSansLinearA-Regular.ttf"
+    "NotoSansLinearB-Regular.ttf"
+    "NotoSansLycian-Regular.ttf"
+    "NotoSansLydian-Regular.ttf"
+    "NotoSansAnatolianHieroglyphs-Regular.ttf"
+    "NotoSansOldItalic-Regular.ttf"
+    "NotoSansRunic-Regular.ttf"
+    "NotoSansOldHungarian-Regular.ttf"
+    "NotoSansGothic-Regular.ttf"
+    "NotoSansElbasan-Regular.ttf"
+    "NotoSansCaucasianAlbanian-Regular.ttf"
+    "NotoSansVithkuqi-Regular.ttf"
+    "NotoSansOgham-Regular.ttf"
+    "NotoSansOldPermic-Regular.ttf"
+    "NotoSansShavian-Regular.ttf"
+    "NotoSansDuployan-Regular.ttf"
+    "NotoSansMayanNumerals-Regular.ttf"
     # Common for all scripts
     "NotoSansSymbols-Regular.ttf"
     "NotoSansSymbols2-Regular.ttf"
---
+++ regional_fonts.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+[[ -z "$VIRTUAL_ENV" ]] && echo "Refusing to run outside of venv. See README.md." && exit 1
+
+python3 -m pip install 'fonttools >= 4.28.5'
+
+# import functions and globals
+source helper.sh
+source categories.sh
+
+# --- execution starts here ---
+mkdir -p cache/
+
+create_tibetan_subset
+drop_vertical_tables NotoSerifDogra-Regular.ttf
+drop_vertical_tables NotoSansNandinagari-Regular.ttf
+drop_vertical_tables NotoSansMongolian-Regular.ttf
+drop_vertical_tables NotoSansNushu-Regular.ttf
+drop_vertical_tables NotoSerifTangut-Regular.ttf
+
+declare -a fonts=(
+    GoNotoAfricaMiddleEast
+    GoNotoSouthAsia
+    GoNotoAsiaHistorical
+    GoNotoSouthEastAsia
+    GoNotoEastAsia
+    GoNotoEuropeAmericas
+)
+
+for font in "${fonts[@]}"; do
+    if [[ -e "$font.ttf" ]]; then
+        echo "Not overwriting existing font $font."
+        continue
+    fi
+    declare -n source_fonts="$font" # nameref to array in categories.sh
+    echo "Generating font $font. Current time: $(date)."
+    go_build "$font.ttf" "${source_fonts[@]}"
+done
+
+create_cjk_iicore
