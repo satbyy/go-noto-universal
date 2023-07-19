c3dc8c1a6174f42dd76f7ed644f3c865334acbd8
Imran Iqbal <i.iqbal@psiphon.ca>
2023-07-19 12:27:48 -0400

    Add GoNotoCurrent-Bold.ttf
    This is best effort as many of the fonts do not have a corresponding
    bold variant. Some of them, like Arabic only have SemiBold.
    
    I have tested this locally (though it is dependant on the build fix pr).
    
    This may also potentially fix: https://github.com/satbyy/go-noto-universal/issues/5

--- categories.sh
+++ categories.sh
@@ -197,6 +197,100 @@ GoNotoCurrent=(
     "NotoMusic-Regular.ttf"
 )
 
+GoNotoCurrentBold=(
+    # It's recommended to put NotoSans-Bold.ttf as the first element in the
+    # list to maximize the amount of meta data retained in the final merged font.
+    # ------- South Asia ---------
+    "NotoSans-Bold.ttf"
+    "NotoSansArabic-SemiBold.ttf" # "NotoNastaliqUrdu-Bold.ttf"
+    "NotoSansBengali-Bold.ttf"
+    "NotoSansChakma-Regular.ttf" # No bold
+    "NotoSansDevanagari-Bold.ttf"
+    "NotoSansGujarati-Bold.ttf"
+    "NotoSansGunjalaGondi-Bold.ttf"
+    "NotoSansGurmukhi-Bold.ttf"
+    "NotoSansKannada-Bold.ttf"
+    "NotoSansLepcha-Regular.ttf" # No bold
+    "NotoSansLimbu-Regular.ttf" # No bold
+    "NotoSansMalayalam-Bold.ttf"
+    "NotoSansMasaramGondi-Regular.ttf" # No bold
+    "NotoSansMeeteiMayek-Regular.ttf" # No bold
+    "NotoSansMro-Regular.ttf" # No bold
+    "NotoSansNewa-Regular.ttf" # No bold
+    "NotoSansOlChiki-Bold.ttf"
+    "NotoSansOriya-Bold.ttf"
+    "NotoSansSaurashtra-Regular.ttf" # No bold
+    "NotoSansSinhala-Bold.ttf"
+    "NotoSansTamil-Bold.ttf"
+    "NotoSansTangsa-Bold.ttf"
+    "NotoSansTelugu-Bold.ttf"
+    "NotoSansThaana-Bold.ttf"
+    "NotoSerifTibetanSubset-Bold.ttf"
+    "NotoSerifToto-Bold.ttf"
+    "NotoSansWancho-Regular.ttf" # No bold
+    "NotoSansWarangCiti-Regular.ttf" # No bold
+    # SE Asia
+    "NotoSansBalinese-Bold.ttf"
+    "NotoSansBatak-Regular.ttf" # No bold
+    "NotoSansBuginese-Regular.ttf" # No bold
+    "NotoSansBuhid-Regular.ttf" # No bold
+    "NotoSansCham-Bold.ttf"
+    "NotoSansHanifiRohingya-Bold.ttf"
+    "NotoSansHanunoo-Regular.ttf" # No bold
+    "NotoSansJavanese-Bold.ttf"
+    "NotoSansKayahLi-Bold.ttf"
+    "NotoSansKhmer-Bold.ttf"
+    "NotoSansLao-Bold.ttf"
+    "NotoSansMyanmar-Bold.ttf"
+    "NotoSansNewTaiLue-Bold.ttf"
+    "NotoSansPahawhHmong-Regular.ttf" # No bold
+    "NotoSansPauCinHau-Regular.ttf" # No bold
+    "NotoSansRejang-Regular.ttf" # No bold
+    "NotoSansSundanese-Bold.ttf"
+    "NotoSansTagalog-Regular.ttf" # No bold
+    "NotoSansTagbanwa-Regular.ttf" # No bold
+    "NotoSansTaiLe-Regular.ttf" # No bold
+    "NotoSansTaiTham-Bold.ttf"
+    "NotoSansTaiViet-Regular.ttf" # No bold
+    "NotoSansThai-Bold.ttf"
+    "NotoSansLisu-Bold.ttf"
+    # 'GoNotoEuropeAmericas.ttf': [
+    "NotoSansArmenian-Bold.ttf"
+    "NotoSansCherokee-Bold.ttf"
+    "NotoSansCoptic-Regular.ttf" # No bold
+    "NotoSansDeseret-Regular.ttf" # No bold
+    "NotoSansGeorgian-Bold.ttf"
+    "NotoSansOsage-Regular.ttf" # No bold
+    # 'GoNotoAfricaMiddleEast.ttf': [
+    "NotoSansAdlam-Bold.ttf"
+    "NotoSansBassaVah-Bold.ttf"
+    "NotoSansHebrew-Bold.ttf"
+    "NotoSansSyriac-Black.ttf"
+    "NotoSansSamaritan-Regular.ttf" # No bold
+    "NotoSansMandaic-Regular.ttf" # No bold
+    "NotoSerifYezidi-Bold.ttf"
+    "NotoSansEthiopic-Bold.ttf"
+    "NotoSansOsmanya-Regular.ttf" # No bold
+    "NotoSansTifinagh-Regular.ttf" # No bold
+    "NotoSansNKoUnjoined-Bold.ttf"
+    "NotoSansVai-Regular.ttf" # No bold
+    "NotoSansMendeKikakui-Regular.ttf" # No bold
+    "NotoSansMedefaidrin-Bold.ttf"
+    # 'GoNotoEastAsia.ttf': [
+    "NotoSansCJKjpSubset-Bold.ttf"
+    "NotoSansCJKkrSubset-Bold.ttf"
+    "NotoSansCJKscSubset-Bold.ttf"
+    "NotoSansMongolianSubset-Regular.ttf" # No bold
+    "NotoSansYi-Regular.ttf" # No bold
+    "NotoTraditionalNushuSubset-Bold.ttf" # Not exactly contemporary use but just 402 glyphs
+    "NotoSansMiao-Regular.ttf" # No bold
+    # Common for all scripts
+    "NotoSansSymbols-Bold.ttf"
+    "NotoSansSymbols2-Regular.ttf" # No bold
+    "NotoSansMathSubset-Regular.ttf" # No bold
+    "NotoMusic-Regular.ttf" # No bold
+)
+
 GoNotoAfricaMiddleEast=(
     "NotoSans-Regular.ttf"
     "NotoNaskhArabic-Regular.ttf" # or "NotoSansArabic-Regular.ttf"
